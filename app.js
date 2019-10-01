c57b3ad396edba048cd1fd23d255c8a2ae07e501
Caleb Pendergraft <cackmed@gmail.com>
2019-10-01 10:27:58 -0700

    removed unnessary files, linked style.css file to index.html, and added a few css changes to the index.html

--- app.js
+++ app.js
@@ -9,14 +9,39 @@ myButton.onclick = function() {
     const myConfirmation = confirm('do you want to take my quiz?');
     console.log(myConfirmation);
 
-    const userAnswer = prompt('is my favorite color blueish? (yes/no');
+    const userAnswer = prompt('What is you Name?')
     const trimedLowercaseUserAnswer = userAnswer.toLowerCase().trim();
     console.log(userAnswer);
+    
+    const userAnswer2 = prompt('Have I ever had a pet rock? (yes/no)');
+    const trimedLowercaseUserAnswer2 = userAnswer2.toLowerCase().trim();
+    console.log(userAnswer);
 
-    if (trimedLowercaseUserAnswer === 'yes')
-        alert('right O');
+    if (trimedLowercaseUserAnswer2 === 'yes')
+        alert('bad start');
     else {
-        alert('nahhh');
+        alert('That is correct');
     }
-    isCorrect(trimedLowercaseUserAnswer);
+    isCorrect(trimedLowercaseUserAnswer2);
+
+    const userAnswer3 = prompt('Did I sell products on Amazon? (yes/no)');
+    const trimedLowercaseUserAnswer3 = userAnswer3.toLowerCase().trim();
+    console.log(userAnswer3);
+    if (trimedLowercaseUserAnswer3 === 'yes')
+        alert('Incredible');
+    else {
+        alert('Sadly that is wrong');
+    }
+    isCorrect(trimedLowercaseUserAnswer3);
+    
+    const userAnswer4 = prompt('Was I born in Portland? (yes/no)');
+    const trimedLowercaseUserAnswer4 = userAnswer4.toLowerCase().trim();
+    console.log(userAnswer4);
+    if (trimedLowercaseUserAnswer4 === 'yes')
+        alert('how did you know!!?!?');
+    else {
+        alert('I am afraid that is incorrect');
+    }
+    isCorrect(trimedLowercaseUserAnswer3);
+
 };
